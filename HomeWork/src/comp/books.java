e1bb6f167b9f38c712fd80bff33d5cbf5f2b584a
Msys <Msys@192.168.1.7>
2019-02-19 18:06:18 +0200

    shit

---
+++ HomeWork/src/comp/books.java
@@ -0,0 +1,26 @@
+
+public class books {
+	private String bookName;
+	private int bookprice;
+	private int count =1 ;
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public int getBookprice() {
+		return bookprice;
+	}
+	public void setBookprice(int bookprice) {
+		this.bookprice = bookprice;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+
+}
---
+++ HomeWork/src/comp/sc.java
@@ -0,0 +1,51 @@
+
+import java.util.ArrayList;
+
+public class sc {
+ private int count ;
+ private int Amount;
+  ArrayList<books> x = new ArrayList<books>();
+   public ArrayList<books> get_X(){
+	  
+	return x;
+	  
+  }
+	public void count() {
+		this.setCount(x.size());
+		}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public int getAmount() {
+		return Amount;
+	}
+	public void setAmount(int amount) {
+		Amount = amount;
+	}
+	public void addbook(books b) {
+		this.x.add(b);
+		this.count();
+		this.totalprice();
+		
+	}
+	public void totalprice() {
+		int adder = 0;
+		for(int i=0;i<x.size();i++) {
+		adder = adder+this.x.get(i).getBookprice()*this.x.get(i).getCount();
+		}
+		this.setAmount(adder);	
+		
+	}
+	public void deleteBook(int i) {
+	x.remove(i);
+	this.count();
+	this.totalprice();
+	
+	}
+}
+
+
+
